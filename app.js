36ae306ba9b4f7934ac5ae521167fc2f9351ffb0
Todd <twitzel@gmail.com>
2016-09-13 13:29:47 -0600

    Added mongo library

--- app.js
+++ app.js
@@ -2,6 +2,36 @@
  * Created by steve on 9/7/2016 .
  */
 
-webserver = require('./webserver.js')
+// LOCAL LIBRARY has the functions unique to this project.
+// we use it to start mongo and retrieve the settings object
 
-webserver.start(function(rslt){console.log(rslt)});
+
+//Initialization Secion
+// ll - llib is the local library
+ll = require('./llib.js');
+// start mongo and load a settings object - passing the database name , and a call back
+ll.startmongo('theatersettings',mongostarted);
+
+
+// all initialization that needs mongo started goes here
+function mongostarted(returnedsettings){
+    global.settings = returnedsettings;
+    // no settings found - init settings
+    if (!settings.type){
+        settings.type = "settings";
+        settings.things = [];
+        console.error("Gatherer Settings missing attempting to create the database...");
+        db.collection('settings').insert(settings, function (err, res) {
+            console.log("Gatherer settings created " + res);
+            console.log("please RESTART");
+            // exit node with error
+            process.exit(1);
+        });
+    } else
+    {
+      // the settings object is loaded - init whatever else
+
+
+
+    }
+}
---
+++ llib.js
@@ -0,0 +1,197 @@
+/**
+ * LOCAL LIBRARY
+ * Created by todd on 3/16/2016.
+ */
+var request = require('request');
+
+MC = require('mongodb').MongoClient;
+
+
+
+
+
+exports.startmongo = function(collectionname,callback) {
+// starts the mongo collection
+// returns the settings object from the database
+
+    MC.connect('mongodb://localhost:27017/'+collectionname, function (err, db) {
+
+        if (!err) {
+            console.log("MongoDB Connected:" + db);
+            global.db = db;
+            //load setting database into memory
+
+            db.collection('settings').findOne({"type": "settings"}, function (err, result) {
+                if (result) {
+                    console.log("Gatherer Settings loaded");
+                    callback(result);
+                } else {
+                    //retrun an empty object
+                    callback({});
+                }
+            });
+
+
+        } else {
+            console.log("No mongo connection")
+        }
+
+
+    });
+};
+exports.savesettings = function(){
+    db.collection('settings').update({'type':'settings'},settings,{upsert:true, w:1},function(err,res){
+        //console.log('Gatherer Setting updated');
+        if (err){
+            console.log('failure writing settings to mongo -- aborting');
+            // exit node with error
+            process.exit(1);
+        }
+    });
+
+};
+exports.getthingbyid = function(inid){
+    var returnvalue = false;
+
+
+    settings.things.some(function(e){
+        if (e.id == inid){
+            returnvalue = e;
+            return true;
+        }
+    });
+
+    return returnvalue;
+};
+exports.getthingbystid = function(inid){
+    var returnvalue = false;
+
+
+    settings.things.some(function(e){
+        if (e.stid == inid){
+            returnvalue = e;
+            return true;
+        }
+    });
+
+    return returnvalue;
+};
+exports.writething = function(obj,savesettings,dontmerge){
+    // This adds or updates an object to the settings
+
+    if (!obj.id){return false;}
+
+    // update whatever webpages with the new data
+    if(typeof(websock) != 'undefined'){
+        websock.send(JSON.stringify({object:"settings.things",data:settings.things}));
+    }
+
+
+    var indexval = -1;
+    settings.things.some(function(e,index){
+        if (e.id == obj.id){
+            indexval = index;
+            return true;
+        }
+    });
+    if (indexval != -1 && !dontmerge){
+
+
+        for (var prop in obj){
+            settings.things[indexval][prop] = obj[prop];
+            //   console.log( obj[prop])
+        }
+//        settings.things[indexval] = obj;
+
+    }else if (indexval != -1 && dontmerge){
+        // just skip - already on entry
+        return;
+
+
+    }
+    else// add item
+    {
+        settings.things.push(obj);
+        console.log('added object');
+    }
+    if (savesettings){
+        console.log('save settings')
+        this.savesettings();
+    }
+
+};
+exports.executecommand = function(obj,command,value,delay){
+    if (delay){
+        setTimeout(function(obj,command,value){ll.executecommand(obj,command,value)},delay,obj,command,value);
+
+
+        console.log('delay command');
+        return;
+    }
+    console.log('exec command');
+
+    if (typeof(obj) != "object"){
+        obj=this.getthingbyid();
+
+
+    }
+
+
+
+    var cmd = false;
+
+    console.log("execute command:"+command+" val:"+value+' name:'+obj.name);
+    obj.commands.some(function(e) {
+        if (e.name == command) {
+            cmd = e;
+            return true;
+        }
+
+
+    });
+    if (cmd){
+        if (cmd.sendto == 'vantage'){
+            serial.write(cmd.command.replace("{value}",value));
+
+        } else
+        if (cmd.sendto == "smartthings"){
+//
+            var request_options = {
+                headers: { Authorization: 'Bearer ' + stsettings.smartthingsoauthtoken}
+            };
+            request_options.uri = stsettings.restUri+ '/update';
+            request_options.json = {
+                id: obj.stid,// vantage light aa look at me
+                command: command,
+                value: Number(value)
+            };
+            request(request_options,function(error, response, body){
+                console.log('command sent to StartThings');
+            });
+
+            //
+
+        } else if (cmd.sendto == "logitec-harmony"){
+
+            console.log('command sent to harmony');
+            hc.sendcommand2(cmd.command);
+
+
+
+
+
+        } else
+        {
+            console.log('unknown sendto:'+sendto)
+
+        }
+
+
+
+    }
+
+
+};
+
+
+
